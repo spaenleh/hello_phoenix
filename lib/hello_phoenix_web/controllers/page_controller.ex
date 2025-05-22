6a66c90cbce8748f81a75b1431948b3df14a310c
spaenleh <spaenleh@gmail.com>
2025-05-22 07:28:57 +0200

    fix: home page html to display links to our controller

--- lib/hello_phoenix_web/controllers/page_controller.ex
+++ lib/hello_phoenix_web/controllers/page_controller.ex
@@ -3,7 +3,6 @@ defmodule HelloPhoenixWeb.PageController do
 
   def home(conn, _params) do
     conn
-    |> put_flash(:error, "Let's pretend we have an error")
     |> render(:home, layout: false)
   end
 
